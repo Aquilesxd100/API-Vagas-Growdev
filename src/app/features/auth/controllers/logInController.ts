1a7e6a10f26fa15c87a2e60f1691123e3eb48568
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-01 10:20:56 -0300

    melhoria sistema de catch dos erros

--- src/app/features/auth/controllers/logInController.ts
+++ src/app/features/auth/controllers/logInController.ts
@@ -9,14 +9,19 @@ export default async function logInController(req: Request, res : Response) {
 
         const loggedAccountInfos : LogInAccountType = await findAccountUC(username, password);
         const generatedToken : string = newTokenGeneratorUC(loggedAccountInfos);
-    
+        
         return res.status(200).send({
             message: "Login realizado com sucesso!",
             token: generatedToken
         });
     } catch(error : any) {
-        return res.status(error.code || 400).send({
-            message: error.message || "Erro!"
+        if(error.code) {
+            return res.status(error.code).send({
+                message: error.message
+            });
+        };
+        return res.status(400).send({
+            message: "Erro!"
         });
     };
 };
