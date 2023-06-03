462a5ec5a04b20e35114b53849849e935d452439
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-03 00:57:21 -0300

    criacao de rota para desativar vaga + recursos

---
+++ src/app/features/recruiter/controllers/deactivateJobController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import deactivateJobUC from "../usecases/deactivateJobUC";
+
+export default async function deactivateJobController
+(req : Request, res : Response) {
+    try {
+        const loggedUserInfos = req.body.loggedUserInfos;
+        const currentJob = req.body.currentJob;
+        await deactivateJobUC(loggedUserInfos, currentJob);
+
+        return res.status(200).send({
+            message: "Vaga desativada com sucesso."
+        });        
+        
+    } catch (error : any) {
+        if (error.code) {
+            return res.status(error.code).send({
+                message: error.message
+            });
+        };
+        return res.status(400).send({
+            message: "Erro!"
+        });
+    };
+};
--- src/app/features/recruiter/routes/recruiterRoutes.ts
+++ src/app/features/recruiter/routes/recruiterRoutes.ts
@@ -4,6 +4,7 @@ import deleteJobController from "../controllers/deleteJobController";
 import getRecruiterJobsController from "../controllers/getRecruiterJobsController";
 import getCandidatesByJobIdController from "../controllers/getCandidatesByJobIdController";
 import activateJobController from "../controllers/activateJobController";
+import deactivateJobController from "../controllers/deactivateJobController";
 
 export const recruiterRoutes = Router();
 
@@ -12,6 +13,6 @@ recruiterRoutes.delete("/deletejob/:jobid", deleteJobController);
 recruiterRoutes.get("/candidates/:jobid", getCandidatesByJobIdController);
 recruiterRoutes.get("/createdjobs", getRecruiterJobsController);
 recruiterRoutes.put("/activatejob/:jobid", activateJobController);
-recruiterRoutes.put("/deactivatejob/:jobid", []);
+recruiterRoutes.put("/deactivatejob/:jobid", deactivateJobController);
 recruiterRoutes.put("/approveapplication/:jobid/:userid", []);
 recruiterRoutes.put("/refuseapplication/:jobid/:userid", []);
