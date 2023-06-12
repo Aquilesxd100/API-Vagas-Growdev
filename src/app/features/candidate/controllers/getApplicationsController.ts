14d6d1f165c00ba7efc5319f1365c388e9371200
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-12 19:19:12 -0300

    att de rota para pegar aplicacoes de candidato

--- src/app/features/candidate/controllers/getApplicationsController.ts
+++ src/app/features/candidate/controllers/getApplicationsController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import getApplicationsUC from "../usecases/getApplicationsUC";
+import { redisRepository } from "../../../shared/repositories/cacheRepository";
 
 export default async function getApplicationsController
 (req: Request, res: Response) {
     try {
         const loggedUserInfos = req.body.loggedUserInfos;
-        const userApplications = await getApplicationsUC(loggedUserInfos);
+        const userApplications = await redisRepository.getApplicationsByCandidateId(loggedUserInfos.loggedUser.id) || await getApplicationsUC(loggedUserInfos);
 
         return res.status(200).send(userApplications);
 
