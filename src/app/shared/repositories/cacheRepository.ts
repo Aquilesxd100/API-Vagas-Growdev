fe03f23760be3d98be58f4a88556fff6bb9c5ba9
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-12 11:03:07 -0300

    novas funcoes no repository

--- src/app/shared/repositories/cacheRepository.ts
+++ src/app/shared/repositories/cacheRepository.ts
@@ -1,16 +1,61 @@
 import { Redis } from "ioredis";
 import { redis } from "../../../main/config/redisconfig";
+import { CandidateEntity } from "../entities/candidate.entity";
+import { RecruiterEntity } from "../entities/recruiter.entity";
 
 class CacheRedisRepository {
     private repository : Redis = redis;
 
     async getAllCandidates() {
-        return await this.repository.get("all-candidates");
+        const allCandidates : null | string = await this.repository.get("all-candidates");
+        if (allCandidates) {
+            return JSON.parse(allCandidates);
+        };
+        return allCandidates;
+    };
+
+    async getCandidateByUsername(username : string) {
+        const candidates : null | Array<CandidateEntity> = await this.getAllCandidates();
+        if (candidates) {
+            return candidates.find((candidate) => candidate.userName === username);
+        };
+        return candidates;
+    };
+
+    async setCandidates(candidatesList : Array<CandidateEntity>) {
+        const processedList = JSON.stringify(candidatesList);
+        await this.repository.set("all-candidates", processedList);
+    };
+
+    async invalidCandidates() {
+        await this.repository.del("all-candidates");
     };
 
     async getAllRecruiters() {
-        return await this.repository.get("all-recruiters");
+        const allRecruiters : null | string = await this.repository.get("all-recruiters");
+        if (allRecruiters) {
+            return JSON.parse(allRecruiters);
+        };
+        return allRecruiters;
     };
+
+    async getRecruiterByUsername(username : string) {
+        const recruiters : null | Array<RecruiterEntity> = await this.getAllRecruiters();
+        if (recruiters) {
+            return recruiters.find((recruiter) => recruiter.userName === username);
+        };
+        return recruiters;
+    };
+
+    async setRecruiters(recruitersList : Array<RecruiterEntity>) {
+        const processedList = JSON.stringify(recruitersList);
+        await this.repository.set("all-recruiters", processedList);
+    };
+
+    async invalidRecruiters() {
+        await this.repository.del("all-recruiters");
+    };
+
 };
 
 export const redisRepository = new CacheRedisRepository;
