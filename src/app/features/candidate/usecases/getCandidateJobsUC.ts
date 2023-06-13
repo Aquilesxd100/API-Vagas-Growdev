eb93063aa80c623548c6231077030b47b5f4c514
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-13 09:55:32 -0300

    adicao de redis na rota de jobs do candidato

--- src/app/features/candidate/usecases/getCandidateJobsUC.ts
+++ src/app/features/candidate/usecases/getCandidateJobsUC.ts
@@ -1,11 +1,17 @@
 import Job from "../../../models/Job";
+import { redisRepository } from "../../../shared/repositories/cacheRepository";
 import { jobsRepository } from "../../../shared/repositories/jobsTypeOrmRepository";
 import { JobsQueriesType } from "../../../shared/types/types";
 
 
 export default async function getCandidateJobsUC
 (queries : JobsQueriesType) : Promise<Array<Job>> {
-    let jobs : Array<Job> = await jobsRepository.getAllJobs();
+    let jobs : Array<Job> | null = await redisRepository.getAllJobs();
+    if (!jobs) {
+        jobs = await jobsRepository.getAllJobs();
+        await redisRepository.setAllJobs(jobs);
+    };
+
     if (queries.search) {
         jobs = jobs.filter((job) => {
             if (job.companyName?.indexOf(queries.search as string) !== -1) {
